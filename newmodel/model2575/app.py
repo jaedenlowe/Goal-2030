6771df306032b821ac5e7db6c0d15daab0e8c2ab
jaedenlowe <lowexinhui@gmail.com>
2024-08-26 11:23:24 +0800

    Update app.py

--- newmodel/model2575/app.py
+++ newmodel/model2575/app.py
@@ -3,17 +3,17 @@ import pandas as pd
 from pycaret.classification import load_model, predict_model
 
 # Load the PyCaret model (make sure to replace 'model_name' with your actual model name)
-model1 = load_model('model_y1_tradkeeper.1')
-model2 = load_model('model_y2_sweeperkeeper.1')
-model3 = load_model('model_y3_ballplayingdefender.1')
-model4 = load_model('model_y4_nononsensedefender.1')
-model5 = load_model('model_y5_fullback.1')
-model6 = load_model('model_y6_allactionmidfielder.1')
-model7 = load_model('model_y7_midfieldplaymaker.1')
-model8 = load_model('model_y8_traditionalwinger.1')
-model9 = load_model('model_y9_invertedwinger.1')
-model10 = load_model('model_y10_goalpoacher.1')
-model11 = load_model('model_y11_targetman.1')
+model1 = load_model('model_y1_tradkeeper')
+model2 = load_model('model_y2_sweeperkeeper')
+model3 = load_model('model_y3_ballplayingdefender')
+model4 = load_model('model_y4_nononsensedefender')
+model5 = load_model('model_y5_fullback')
+model6 = load_model('model_y6_allactionmidfielder')
+model7 = load_model('model_y7_midfieldplaymaker')
+model8 = load_model('model_y8_traditionalwinger')
+model9 = load_model('model_y9_invertedwinger')
+model10 = load_model('model_y10_goalpoacher')
+model11 = load_model('model_y11_targetman')
 
 
 # Streamlit app
